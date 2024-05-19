d330f4b0f6dc2beb7867d44e41d36f59215e767d
Lai Yi Wen <christine@Lais-MacBook-Air.local>
2024-05-19 17:47:40 +0800

    login and sign up

--- main.py
+++ main.py
@@ -1,21 +1,8 @@
-from flask import Flask, request, render_template
+# main.py
+from src import create_app
 
-app = Flask(__name__)
-
-@app.route('/')
-def login():
-    return render_template('login.html')  # Assuming your HTML file is named 'login.html' and located in the 'templates' directory
-
-@app.route('/submit', methods=['POST'])
-def submit():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        # Here you should implement your validation logic and check against your user database
-        if email == "example@example.com" and password == "securepassword123":
-            return "Login successful!"
-        else:
-            return "Invalid email or password!"
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
+
